10cd1c895cd0cf08289da13f8b4ad60e30b4f77e
Surya-Kaliappan <kaliappansurya47@gmail.com>
2026-02-25 20:33:11 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,10 @@ int main() {
     std::getline(std::cin, command);
     if(command == "exit"){
       break;
+    } else if (command.substr(0,command.find(" ")) == "echo") {
+      std::cout << command.substr(command.find(" ")+1) << std::endl;
+    } else {
+      std::cout << command << ": command not found" << std::endl;
     }
-    std::cout << command << ": command not found" << std::endl;
   }
 }
