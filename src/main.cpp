99056495b873848545615cd39d1237cc4eb23d45
Surya-Kaliappan <kaliappansurya47@gmail.com>
2026-02-28 03:29:16 +0000

    feat: implement logic for type command

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include <string>
 
+std::string getHead(std::string command) {
+  return command.substr(0,command.find(" "));
+}
+
+std::string getBody(std::string command) {
+  return command.substr(command.find(" ")+1);
+}
+
+void checkType(std::string command) {
+  std::string arg = getBody(command);
+  if(arg == "echo") {
+    std::cout << "echo is a shell builtin";
+  } else if (arg == "exit") {
+    std::cout << "exit is a shell builtin";
+  } else if (arg == "type") {
+    std::cout << "type is a shell builtin";
+  } else {
+    std::cout << arg+": not found";
+  }
+  std::cout << std::endl;
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+
 
   // TODO: Uncomment the code below to pass the first stage
   while(true){
@@ -14,8 +37,10 @@ int main() {
     std::getline(std::cin, command);
     if(command == "exit"){
       break;
-    } else if (command.substr(0,5) == "echo ") {
+    } else if (getHead(command) == "echo") {
       std::cout << command.substr(5) << std::endl;
+    } else if (getHead(command) == "type") {
+      checkType(command);
     } else {
       std::cout << command << ": command not found" << std::endl;
     }
