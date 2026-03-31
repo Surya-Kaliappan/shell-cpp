345882fd80dae71e616e1d8e73184e81f03563d5
Surya-Kaliappan <kaliappansurya47@gmail.com>
2026-03-31 23:39:46 +0000

    feat: run_ext_program

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <cstdlib> // for getenv
 #include <sstream> // for string stream
 #include <unistd.h> // for access() and X_OK
+#include <sys/wait.h> // for waitpid()
 
+// funtion to split the command and arguments
 void parseCommand(const std::string& input, std::string& cmd, std::string& args) {
   size_t space_pos = input.find(' ');
 
@@ -16,6 +19,7 @@ void parseCommand(const std::string& input, std::string& cmd, std::string& args)
   }
 }
 
+// function to find the either type or executable file path
 void checkType(const std::string& args) {
   if(args.empty()) return ;
   
@@ -39,6 +43,37 @@ void checkType(const std::string& args) {
   }
 
   std::cout << args << ": not found\n";
+}
+
+// function to execute external programs
+void executeExternal(const std::string& cmd, const std::string& args_str) {
+  std::vector<std::string> args_list;
+  args_list.push_back(cmd);
+
+  std::stringstream ss(args_str);
+  std::string token;
+  while(ss >> token) {
+    args_list.push_back(token);
+  }
+
+  std::vector<char*> c_args;
+  for(size_t i = 0; i < args_list.size(); i++) {
+    c_args.push_back(const_cast<char*>(args_list[i].c_str()));
+  }
+  c_args.push_back(nullptr);
+
+  pid_t pid = fork();
+  
+  if(pid == 0) {
+    execvp(c_args[0], c_args.data()); // This will execute the command in child process
+    std::cout << cmd << ": command not found\n"; // if the process success then this line won't work unless failed
+    exit(1);
+  } else if(pid > 0) {
+    int status;  // this vaiable will hold the exit status of the child process
+    waitpid(pid, &status, 0); // wait for the child process to finish
+  } else {
+    std::cerr << "Fork failed\n";
+  }
 }
 
 int main() {
@@ -65,8 +100,11 @@ int main() {
     } else if (cmd == "type") {
       checkType(args);
     } else {
-      std::cout << input << ": command not found" << std::endl;
+      // std::cout << input << ": command not found" << std::endl;
+      executeExternal(cmd, args);
     }
+
+    std::cout << std::endl;
   }
 
   return 0;
